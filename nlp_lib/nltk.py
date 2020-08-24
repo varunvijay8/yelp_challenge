0b2ca80d694532c0543c03b015ee2492360b496a
varun <varunvijay8@gmail.com>
2020-08-23 17:49:18 -0700

    added sklearn api, nltk and spacy

--- nlp_lib/nltk.py
+++ nlp_lib/nltk.py
@@ -6,6 +6,8 @@ from nltk import RegexpParser
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+from spacy import spacy
+
 class nltk(object):
 
     def __init__(self):
@@ -16,3 +18,16 @@ class nltk(object):
         nltk.download('stopwords')
         nltk.download('wordnet')
         nltk.download('brown')
+        self._spacy_obj = spacy()
+
+    def chunk_nouns_3(self, sent):
+        lst = []
+        token_tag = self._spacy_obj.spacy_pos_tag_to_nltk(sent)
+        grammar = r"""NP: {(<JJ><CC>)?<JJ>?<NN.*>+}"""
+        noun_parser = RegexpParser(grammar)
+        parsed_output = noun_parser.parse(token_tag)
+        for subtree in parsed_output.subtrees(filter=lambda t: t.label() == 'NP'):
+            lst.append(' '.join(word for (word, pos) in subtree.leaves()))
+        return lst
+
+    
--- nlp_lib/spacy.py
+++ nlp_lib/spacy.py
@@ -3,6 +3,10 @@ import spacy
 class spacy(object):
 
     def __init__(self):
-        pass
+        self._spacy = spacy.load('en')
+
+    def spacy_pos_tag_to_nltk(self, sent):
+        doc = self._spacy(sent)
+        return [(t.text, t.tag_) for t in doc]
 
     
