5945e710f8f3bef9532b45251cda05dee1500f19
varun <varunvijay8@gmail.com>
2020-08-29 19:35:42 -0700

    added global configuration file config.xml for setting project attributes

--- classifier.py
+++ classifier.py
@@ -1,5 +1,16 @@
 from spark.spark_api import spark
 from data.storage_factory import storage_factory
+from bs4 import BeautifulSoup
+
+def get_storage_type():
+    '''
+    Read the global configuration file config.xml to get the storage type
+    '''
+    with open('config.xml') as fp:
+        soup = BeautifulSoup(fp, 'xml')
+        storage_config = soup.find_all('storage_type')
+
+    return storage_config[0].get_text()
 
 if __name__ == "__main__":
 
@@ -7,4 +18,4 @@ if __name__ == "__main__":
     s = spark()
 
     # 2) Create instance of storage class
-    storage = storage_factory.getstorage('local')
+    storage = storage_factory.getstorage(get_storage_type())
