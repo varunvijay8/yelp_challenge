097507bf3f0e5c22f139114a56f2668600c9643d
varun <varunvijay8@gmail.com>
2020-08-29 19:24:32 -0700

    added configuration file for local storage and created instance for local storage in classifier

--- classifier.py
+++ classifier.py
@@ -1,4 +1,10 @@
 from spark.spark_api import spark
+from data.storage_factory import storage_factory
 
 if __name__ == "__main__":
+
+    # 1) Create spark session
     s = spark()
+
+    # 2) Create instance of storage class
+    storage = storage_factory.getstorage('local')
--- data/local_storage_api.py
+++ data/local_storage_api.py
@@ -1,9 +1,21 @@
 from data.storage_interface import storage
+from bs4 import BeautifulSoup
 
 class local_storage(storage):
 
+    def __init__(self):
+        '''
+        Initialize the path using the text from review_json tag in the configuration
+        file local_storage_config.xml
+        '''
+        with open("data/local_storage_config.xml") as fp:
+            soup = BeautifulSoup(fp, 'xml')
+            review_path = soup.find_all('review_json')
+
+        self.set_path(review_path[0].get_text())
+
     def set_path(self, path):
-        pass
+        self._path = path
 
     def get_path(self):
-        pass
+        return self._path
